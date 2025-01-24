51d5c0d95c19aeb6913a1f0f4118726569ecf309
Nicholas James <syncert@gmail.com>
2025-01-24 10:32:10 -0600

    Removed chrome user profile, it's not required.

--- driver_manager.py
+++ driver_manager.py
@@ -13,15 +13,11 @@ class DriverManager:
         chrome_options = uc.ChromeOptions()
         chrome_options.add_argument("--disable notifications") # Disable Popups
 
-        #add location data from user profile to bypass popup, can find by entering chrome://version/ in chrome
-        #TODO: Find out why the pop-up opens at beginning of driver initialization
-        chrome_options.add_argument(r"--user-data-dir=\
-                                    C:\Users\synce\AppData\Local\Google\Chrome\User Data\Default") #Add your dir here
+        #this option disables the pesky location pop ups
         chrome_options.add_experimental_option("prefs", {
             "profile.default_content_setting_values.geolocation": 2,  # Block location requests
         })
         chrome_options.add_argument("--log-level=3")  # Suppress minor logs: 0=ALL, 1=INFO, 2=WARNING, 3=ERROR
-        # chrome_options.add_argument("--silent")  # Suppress most logs
         # Initialize the WebDriver with undetected_chromedriver
         self.driver = uc.Chrome(options=chrome_options) #headless=True
         return self.driver
